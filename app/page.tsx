c510a3712089b597b91ecc4fa0e2028acc6bb100
Jani Behm <jani.behm@gmail.com>
2025-09-08 18:45:46 +0300

    Create the header text section.

--- app/page.tsx
+++ app/page.tsx
@@ -5,45 +5,15 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <h1 className={styles.title}>
+          PORTAL <br/> PORTFOLIO
+        </h1>
+        <p className={styles.description}>
+          I craft digital experiences that blend beautiful design with functionality. 
+          Specializing in modern web applications that scales.
+        </p>
+        <div className={styles.buttonWrapper}>
+          <button className={styles.buttonPrimary}>Download CV</button>
         </div>
       </main>
       <footer className={styles.footer}>
