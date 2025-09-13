af2cc7cae13ea72ec22c0f8c268b8110948e83bb
Jani Behm <jani.behm@gmail.com>
2025-09-13 19:31:04 +0300

    Preload bg image and remove placeholder images.

--- apps/portfolio-portal/app/layout.tsx
+++ apps/portfolio-portal/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" href="/mountains.jpg" as="image" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
