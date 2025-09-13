fc44f735b09bb19a80d39d8ece123ae92f9951dc
Jani Behm <jani.behm@gmail.com>
2025-09-13 18:26:20 +0300

    Add output withNx to next.config.

--- apps/portfolio-portal/next.config.ts
+++ apps/portfolio-portal/next.config.ts
@@ -1,16 +1,18 @@
 import type { NextConfig } from "next";
+import { withNx } from "@nx/next/plugins/with-nx"; 
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = withNx({
+  output: "standalone", 
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
-};
+});
 
 export default nextConfig;
