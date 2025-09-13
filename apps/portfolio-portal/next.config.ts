a9b7a6ad3350e1d070b5e5179931f2504d2866d8
Jani Behm <jani.behm@gmail.com>
2025-09-13 18:58:13 +0300

    Remove output at next.config.

--- apps/portfolio-portal/next.config.ts
+++ apps/portfolio-portal/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import { withNx } from "@nx/next/plugins/with-nx"; 
 
 const nextConfig: NextConfig = withNx({
-  output: "standalone", 
   images: {
     remotePatterns: [
       {
